66335225b437faa61a85ce7d7ac802e16303b222
Chelaev Petr <petrcelaev@gmail.com>
2022-11-03 10:35:28 +0400

    Changes to working links

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from time import sleep
 import lxml
 import random
 from base64 import encode
+import os
+
+
+def create_dir():
+    os.mkdir('dataset/')
+    os.mkdir('dataset/bad')
+    os.mkdir('dataset/good')
+
 
 def site_read():
     list_films = []
@@ -18,8 +26,9 @@ def site_read():
 
     with open('List_Films_2.txt', 'w') as file:
         for i in list_films:
-          file.write('https://www.kinopoisk.ru' + f'{i}' + 'reviews/ord/rating/status\n')
-
+          file.write('https://www.kinopoisk.ru' + f'{i}' + 'reviews\n')
+    
+          
 def new_file(status):
     status = status.replace('/', '')
     file = open('List_Films_2.txt', 'r')
@@ -28,20 +37,20 @@ def new_file(status):
     with open('List_Films_' + status + '.txt', 'w') as file_2:
         for i in line:
             i = i.strip()
-            i += '/' + status + '/perpage/200\n'
-            file_2.write(i)
+            i += '/ord/date/status/' + status + '/perpage/200\n'
+            file_2.write(i)                  
 
-def printim(status):
+def printim_lines(status):
     file = open('List_Films_' + status + '.txt', 'r')
     lines = file.readlines()
     file.close()
-    j = 929
+    j = 0
 
     while (j != 1000):
-        for i in lines:
-            i = i.strip()
+        for line in lines:
+            line = line.strip()
 
-            response = requests.get(i, headers={"User-Agent":"Mozilla/5.0"})
+            response = requests.get(line, headers={'User-Agent':'Mozilla/5.0'})
             print("#" * 100)
             print(response.text)
             print("#" * 100)
@@ -56,8 +65,12 @@ def printim(status):
                 sleep(30)
                 continue
             for review in reviews:
-                if j < 0:
-                    with open('dataset/' + status + '/000' + str(j) + '.txt', 'a', encoding='utf-8') as file:
+               num = '/000'
+               if j >= 10 and j < 100:
+                    num = '/00'
+               if j >= 100 and j < 1001:
+                num = '/0'
+               with open('dataset/' + status +  num + str(j) + '.txt', 'a', encoding='utf-8') as file:
                         name = soup.find(class_='breadcrumbs__link')
                         text = name.text.strip()
                         file.write(text + '\n')
@@ -65,36 +78,18 @@ def printim(status):
                         text_reviews = review.text.strip()
                         file.write(text_reviews)
                         print('...........Downland File №', j, '...........')
-                if j >= 10 and j < 100:
-                    with open('dataset/' + status + '/00' + str(j) + '.txt', 'a', encoding='utf-8') as file:
-                        name = soup.find(class_='breadcrumbs__link')
-                        text = name.text.strip()
-                        file.write(text + '\n')
-                        file.write(soup.find(class_='sub_title').text)
-                        text_reviews = review.text.strip()
-                        file.write(text_reviews)
-                        print('...........Downland File №', j, '...........')
-                if j >= 100 and j < 1001:
-                    with open('dataset/' + status + '/0' + str(j) + '.txt', 'a', encoding='utf-8') as file:
-                        name = soup.find(class_='breadcrumbs__link')
-                        text = name.text.strip()
-                        file.write(text + '\n')
-                        file.write(soup.find(class_='sub_title').text)
-                        text_reviews = review.text.strip()
-                        file.write(text_reviews)
-                        print('...........Downland File №', j, '...........')
-                j += 1
-                if j == 1000:
-                    break
-
+               j += 1
+               
+                
 def main():
+    #create_dir()
     #site_read()
-    #status = 'bad'
+    status = 'bad'
     #new_file(status)
-    #printim(status)
-    status = 'good'
-    new_file(status)
-    printim(status)
+    printim_lines(status)
+    #status = 'good'
+    #new_file(status)
+   # printim_lines(status)
 
 
 if __name__ == '__main__':
