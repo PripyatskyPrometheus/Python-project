df589eb2ccf390a10d77951e43b6363167e698f6
Chelaev Petr <petrcelaev@gmail.com>
2022-12-12 08:01:41 +0400

    class

--- simpleIterator.py
+++ simpleIterator.py
@@ -4,14 +4,14 @@ import re
 
 class SimpleIterator:
 
-    def __init__(self, class_file: str, class_name: str):
+    def __init__(self, file_name: str, class_name: str):
         self.class_name = class_name
         self.list = []
-        self.class_file = class_file
+        self.file_name = file_name
         self.counter = 0
 
-        with open(self.class_file, 'r', encoding='utf-8') as f:
-            reader = csv.reader(f, delimiter='\t')
+        with open(self.file_name, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f, delimiter=' ')
             for row in reader:
                 self.list.append(row)
 
@@ -22,10 +22,7 @@ class SimpleIterator:
 
     def __next__(self) -> str:
         if self.counter < len(self.list):
-            abs_way = re.split(';', str(self.list[self.counter]))
             self.counter += 1
-            nc = self.class_name + ']'
-            if abs_way[2] == nc:
-                return abs_way[0][2:]
+            return str(self.list[self.counter - 1])
         else:
             raise StopIteration
